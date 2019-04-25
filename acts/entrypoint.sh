deca39a2bf777cc873a668f3bc4ac8eddcc93678
Muhammad Usman <me@usmanmuhd.com>
2019-04-25 20:50:32 +0530

    new changes

--- acts/entrypoint.sh
+++ acts/entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-# Collect static files
-echo "Running with hostname"
-echo $HOSTNAME
-
-# Apply database migrations
-echo "Applying database migrations"
 python3 acts/manage.py migrate
-
-# Start server
-echo "Starting server"
 python3 acts/manage.py runserver 0:80
--- users/entrypoint.sh
+++ users/entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-# Collect static files
-echo "Running with hostname"
-echo $HOSTNAME
-
-# Apply database migrations
-echo "Applying database migrations"
 python3 users/manage.py migrate
-
-# Start server
-echo "Starting server"
 python3 users/manage.py runserver 0:80

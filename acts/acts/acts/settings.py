f06130027e6af36c81699de4af458a45191ff44b
Muhammad Usman <me@usmanmuhd.com>
2019-04-25 17:11:01 +0530

    Changes!

--- acts/acts/acts/settings.py
+++ acts/acts/acts/settings.py
@@ -76,24 +76,24 @@ WSGI_APPLICATION = 'acts.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-'''
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 	}
 }
-'''
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'acts',
-		'USER': 'shangrila',
-		'PASSWORD': 'shangrila',
-		'HOST': '3.210.166.12',
-		'PORT': '5432',
-	}
-}
+
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+# 		'NAME': 'acts',
+# 		'USER': 'shangrila',
+# 		'PASSWORD': 'shangrila',
+# 		'HOST': '3.210.166.12',
+# 		'PORT': '5432',
+# 	}
+# }
 
 
 # Password validation
